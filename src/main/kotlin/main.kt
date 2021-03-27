c7c49b03a9bbc1c5846d898ded7e5a52c8dc644b
Mahdi_Farahbakhsh <mahdi.farahbakhsh99@gmail.com>
2021-03-27 09:05:59 +0430

    Pick a random number

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,7 +1,14 @@
 //In His Name
-fun main(args: Array<String>)
+import kotlin.random.Random
+fun main()
 {
+    //ask user to set a upper limit for random number
     println("Enter a higher limit:")
     val upperLimit:Int = readLine()!!.toInt();
     println("Higher Limit is \"$upperLimit\"")
+
+    //pick a random from 1 to upperLimit
+    val randomNumber = Random.nextInt(1,upperLimit)
+    println("Random Number is \"$randomNumber\"")
+
 }
